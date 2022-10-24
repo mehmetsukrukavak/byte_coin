6aaeda87d149ae5b859b968fb00fa7363eef05ea
Mehmet Şükrü Kavak <mehmetsukru.kavak07@gmail.com>
2022-10-24 13:03:18 +0300

    Step 5: Update the User Interface Using the Delegate Pattern

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,7 +8,19 @@
 
 import Foundation
 
+protocol CoinManagerDelegate {
+    
+    //Create the method stubs wihtout implementation in the protocol.
+    //It's usually a good idea to also pass along a reference to the current class.
+    //e.g. func didUpdatePrice(_ coinManager: CoinManager, price: String, currency: String)
+    //Check the Clima module for more info on this.
+    func didUpdatePrice(price: String, currency: String)
+    func didFailWithError(error: Error)
+}
+
 struct CoinManager {
+    
+    var delegate: CoinManagerDelegate?
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
     let apiKey = "515A1294-0819-443F-A831-D3EFA007749D"
@@ -16,48 +28,57 @@ struct CoinManager {
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
     
     func getCoinPrice(for currency: String) {
-           
-           let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
-
-           if let url = URL(string: urlString) {
-               
-               let session = URLSession(configuration: .default)
-               let task = session.dataTask(with: url) { (data, response, error) in
-                   if error != nil {
-                       print(error!)
-                       return
-                   }
-                   
-                   if let safeData = data {
-                       let bitcoinPrice = self.parseJSON(safeData)
-                   }
-                   
-               }
-               task.resume()
-           }
-       }
+        
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        
+        if let url = URL(string: urlString) {
+            
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    
+                    if let bitcoinPrice = self.parseJSON(safeData) {
+                        
+                        //Optional: round the price down to 2 decimal places.
+                        let priceString = String(format: "%.2f", bitcoinPrice)
+                        
+                        //Call the delegate method in the delegate (ViewController) and
+                        //pass along the necessary data.
+                        self.delegate?.didUpdatePrice(price: priceString, currency: currency)
+                    }
+                }
+                
+            }
+            task.resume()
+        }
+    }
     
     func parseJSON(_ data: Data) -> Double? {
+        
+        //Create a JSONDecoder
+        let decoder = JSONDecoder()
+        do {
             
-            //Create a JSONDecoder
-            let decoder = JSONDecoder()
-            do {
-                
-                //try to decode the data using the CoinData structure
-                let decodedData = try decoder.decode(CoinData.self, from: data)
-                
-                //Get the last property from the decoded data.
-                let lastPrice = decodedData.rate
-                print(lastPrice)
-                return lastPrice
-                
-            } catch {
-                
-                //Catch and print any errors.
-                print(error)
-                return nil
-            }
+            //try to decode the data using the CoinData structure
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            
+            //Get the last property from the decoded data.
+            let lastPrice = decodedData.rate
+            print(lastPrice)
+            return lastPrice
+            
+        } catch {
+            
+            //Catch and print any errors.
+            delegate?.didFailWithError(error: error)
+            return nil
         }
+    }
 }
 
 
